7b882b3b8469a8aa766ce90884797f5cf7b97048
balo7773 <balogunabdulmalik04@gmail.com>
2023-08-11 15:28:55 -0700

    betty want to frustrate me

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
- /**
+ /*
   * main - Entry point
   *
   * Return: Always 0 (Success)
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
 
