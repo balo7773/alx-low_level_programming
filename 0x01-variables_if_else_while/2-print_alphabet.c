457166ac95f3ca2dd0f0d15978c752a3ade43171
balo7773 <balogunabdulmalik04@gmail.com>
2023-08-13 16:36:25 -0700

    using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int alpbt;
+
+	for (alpbt = 'a'; alpbt <= 'z'; alpbt++)
+	{
+
+	putchar(me);
+
+	}
+
+
+	return (0);
+
+}
