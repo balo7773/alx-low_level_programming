322c79f1bf8dc8ebe24fef7cb51aa27ab482dcd5
balo7773 <balogunabdulmalik04@gmail.com>
2023-08-13 16:57:47 -0700

    printing alphabets87

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+	int alpbt;
+	int ALPBT;
+
+	for (alpbt = 'a'; alpbt <= 'z'; alpbt++)
+	{
+		putchar(alpbt);
+	}
+
+	for (ALPBT = 'a'; ALPBT <= 'z'; ALPBT++)
+	{
+		putchar(ALPBT);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
+
+
