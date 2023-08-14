38ed614847b72a1c5e18a9c57d79000fdf076759
balo7773 <balogunabdulmalik04@gmail.com>
2023-08-14 04:22:41 -0700

    using put char

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar('0' + num);
+
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		if (num == 9)
+		{
+			putchar('$');
+		}
+	}
+
+	return (0);
+}
