f03df1d43092703da16f5b40b992308ecc636d24
balo7773 <balogunabdulmalik04@gmail.com>
2023-08-13 17:28:31 -0700

    omitting some letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int alpbt;
+
+	for (alpbt = 'a'; alpbt <= 'z'; alpbt++)
+	{
+	if (alpbt != 'q' && alpbt <= 'e')
+	{
+	putchar(alpbt);
+
+	}
+
+	}
+
+
+	putchar('\n');
+
+	return (0);
+
+}
