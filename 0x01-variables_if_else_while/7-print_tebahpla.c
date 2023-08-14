b8d29acde544a23cf59ae8f5bd0473ac6d5b2b07
balo7773 <balogunabdulmalik04@gmail.com>
2023-08-13 19:10:19 -0700

    in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int rev;
+
+	for (rev = 'z'; rev <= 'a'; rev++)
+	{
+		putchar(rev);
+	}
+
+	putchar('\n');
+	return (0);
+}
