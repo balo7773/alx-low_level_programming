d892889757ffef7865f7f2ad3e9c619a303e8128
balo7773 <balogunabdulmalik04@gmail.com>
2023-08-17 12:05:43 -0700

    iterating values

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - function used to iterate nos 14 times
+ * main - Entry point
+ * void: a datatype with no return function
+ * Return: Always 0 (success)
+ */
+
+void more_numbers(void)
+{
+	int outer;
+	int inner;
+
+	for (outer = 0; outer <= 10; outer++)
+	{
+		for (inner = 0; inner < 15; inner++)
+		{
+			if (inner >= 10)	
+			_putchar(49);
+			_putchar(inner % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
