d43d2071f70f0e4fc4b04502abc9e81be90e3a1f
balo7773 <balogunabdulmalik04@gmail.com>
2023-08-17 14:42:10 -0700

    printing squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - a function that uses # to print square
+ * main - Entry point
+ * @size - a variable to store int datatype
+ * Return: Always 0 (success)
+ */
+
+void print_square(int size)
+{
+	int outer;
+	int inner;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (outer = 0; outer < size; outer++)
+		{
+			for (inner = 0; inner < size; inner++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
