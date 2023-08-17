e6571bceeac152385f9da216c85f44a36b26a4cf
balo7773 <balogunabdulmalik04@gmail.com>
2023-08-16 17:41:41 -0700

    printing alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+i#include "main.h"
+/**
+ * print_alphabet_x10 - printing the alphabet 10 times
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int cha = 97;
+	int no = 1;
+
+	while (n < 11)
+	{
+		while (cha <= 122)
+		{
+			_putchar(cha);
+
+			cha++;
+		}
+	_putchar('\n');
+	}
+}
+
