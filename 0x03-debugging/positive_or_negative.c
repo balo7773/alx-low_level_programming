2f1eca14ff5985cea5701f66c5510544a8a91389
balo7773 <balogunabdulmalik04@gmail.com>
2023-08-18 09:54:35 -0700

    testing

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *  positive_or_negative - a function to check if int is pos or neg
+ *  main - Entry point
+ *  @i: int datatype to be checked
+ *  Return: Always 0 (success)
+ */
+void positive_or_negative(int i)
+{
+	if (i < 0)
+	{
+	printf("%d is negative\n", i);
+	}
+	else if (i > 0)
+	{
+	printf("%d is positive\n", i);
+	}
+}
