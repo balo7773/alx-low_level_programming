f6ac85f2142a4edeb915877a556471d4a6abc4ae
balo7773 <balogunabdulmalik04@gmail.com>
2023-08-23 12:16:47 -0700

    program like strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcat - a function like the strcat()
+ * @dest: the destination string
+ * @src: string(src) to append to the dest sting
+ * Return: returns dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i + 1] = src[j];
+	}
+	return (dest);
+}
