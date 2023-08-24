535e80f525fb220089ec99dbb1b5420eb106540a
balo7773 <balogunabdulmalik04@gmail.com>
2023-08-24 09:36:05 -0700

    copying n characters

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strncpy - a function used to copy a string
+ * @dest: a string variable
+ * @src: a string variable
+ * @n: no of char to be copied(in int)
+ * Return: dest (a string)
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != 0; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
