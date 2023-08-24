f15b5b4d3fee64328aec4f62a3a6aa683fc0610f
balo7773 <balogunabdulmalik04@gmail.com>
2023-08-24 13:52:42 -0700

    capitalizing

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+  * cap_string - a function to caps all first words of string
+  * @caps: a string variable
+  * Return: return caps(string)
+  */
+char *cap_string(char *caps)
+{
+	int i;
+	int j;
+	char lower;
+
+	char str[13] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	for (i = 0; caps[i]; i++)
+	{
+		for (j = 0; j < 13; j++)
+		{
+			lower = (caps[i] >= 97 && caps[i] <= 122);
+
+			if (caps[i - 1] == str[j] && lower)
+			{
+			caps[i] = caps[i] - 32;
+			}
+		}
+	}
+	return (caps);
+}
