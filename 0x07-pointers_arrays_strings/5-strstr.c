ee5643427530e3d49e27cf8df2a9c262198bfd9c
balo7773 <balogunabdulmalik04@gmail.com>
2023-08-28 16:37:36 -0700

    still on strings

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i;
+	unsigned int j;
+
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+			{
+				break;
+			}
+		}
+		if (needle[j] == '\0')
+		{
+			return (&haystack[i]);
+		}
+	}
+
+	return ('\0');
+}
