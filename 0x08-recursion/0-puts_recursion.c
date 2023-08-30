5558a159d4197b6ee66500a2c30b5d884a155b8c
balo7773 <balogunabdulmalik04@gmail.com>
2023-08-30 13:18:29 -0700

    print string recursively

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion: a function like the puts func
+ * @s: a variable that points to a string
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
