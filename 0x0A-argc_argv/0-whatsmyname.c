f0de1a70b7ec15b99ab067c4361ef88babff7f63
balo7773 <balogunabdulmalik04@gmail.com>
2023-09-01 18:30:12 -0700

    learning commasnd line arguement

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string and a new line
+ * @argc: datatype int
+ * @argv: datatype string
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
