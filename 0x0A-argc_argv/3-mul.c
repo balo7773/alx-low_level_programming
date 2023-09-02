3f07d05af4e15d1dc0bfc5d52d368c7b77c77215
balo7773 <balogunabdulmalik04@gmail.com>
2023-09-01 19:02:44 -0700

    mul of two nos

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - output the multiplication of two numbers
+ * @argc: int datatype
+ * @argv: pointer to the char datatype
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+	int mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	mul = num1 * num2;
+
+	printf("%d\n", mul);
+
+	return (0);
+}
