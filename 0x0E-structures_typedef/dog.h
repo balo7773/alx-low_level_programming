6e66b191e26256f711a441b4806605fc49e4ce08
balo7773 <balogunabdulmalik04@gmail.com>
2023-09-12 19:03:40 -0700

    learning structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - features of dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Description: dog features
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
