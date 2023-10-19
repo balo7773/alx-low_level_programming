2ec0c8d5e301e76283055a037d455bc1c586cd38
Balogun Abdulmalik <balogunabdulmalik04@gmail.com>
2023-10-19 07:52:05 -0700

    learning list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * add_node - func that adds a new node at the start of a list
+ *@head: head pointer
+ *@str: str
+ *
+ * Return: addrs
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	char *dup;
+	int len;
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	dup = strdup(str);
+	if (dup == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	for (len = 0; str[len];)
+		len++;
+
+	new->str = dup;
+	new->len = len;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+
+}
