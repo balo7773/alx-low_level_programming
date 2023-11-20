5a93329b473b99d9b40c53bc17c6127339931b36
balogun abdulmalik <balogunabdulmalik04@gmail.com>
2023-11-20 13:26:35 +0100

    learning functions and nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,56 +1,22 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 /**
- * print_to_98 - func to print to 98
- * @n: int datatype
- * return: void
+ * print_to_98 - print all natural numbers from n to 98.
+ * @n: the number to start counting from n to 98
+ * Return: Always 0.
  */
 void print_to_98(int n)
 {
-	int i;
-	if (n == 98)
-	{
-		printf("%d\n", n);
-
-	}
-	if (n == 0)
-	{
-		for(i = n; i <= 97; i++)
-		{
-			printf("%d, ", i);
-			if (i == 98)
-			{
-				printf("%d", i);
-			}
-		}
-		_putchar('\n');
-	}
-	else if (n > 98)
-	{
-		for (i = n; i >= 98; i--)
-		{
-			if (i > 98 && i != 98)
-			{
-				printf("%d, ", i);
-			}
-			else if (i == 98)
-			{
-				printf("%d", i);
-			}
-		}
-		_putchar('\n');
-	}
-	else if (n < 98)
-	{
-		for (i = n; i <= 97; i++)
-		{
-			printf("%d, ", i);
-		}
-		if (i == 98)
-			{
-				printf("%d", i);
-			}
-		_putchar('\n');
-	}
+    if (n < 98)
+    {
+        for (n = n; n < 98; n++)
+        printf("%d, ", n);
+        printf("%d\n", 98);
+    }
+    else
+    {
+        for (n = n; n > 98; n--)
+        printf("%d, ", n);
+        printf("%d\n", 98);
+    }
 }
-
