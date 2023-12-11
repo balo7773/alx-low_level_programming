406140b61144ef2ab508e78f3cf18e65fdcff473
balogun abdulmalik <balogunabdulmalik04@gmail.com>
2023-12-11 23:12:26 +0100

    learning dynamic library

---
+++ 0x18-dynamic_libraries/arithmetic.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+
+/**
+ * add - func to add
+ * @a: int datatype
+ * @b: int datatype
+ * Return: add
+*/
+int add(int a, int b)
+{
+    return (a + b);
+}
+/**
+ * add - func to sub
+ * @a: int datatype
+ * @b: int datatype
+ * Return: sub
+*/
+int sub(int a, int b)
+{
+    return (a - b);
+}
+/**
+ * mod - func to mod
+ * @a: int datatype
+ * @b: int datatype
+ * Return: mod
+*/
+int mod(int a, int b)
+{
+    return (a % b);
+}
+/**
+ * mul - func to add
+ * @a: int datatype
+ * @b: int datatype
+ * Return: mul
+*/
+int mul(int a, int b)
+{
+    return (a * b);
+}
---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  - func to div
+ * @a: int datatype
+ * @b: int datatype
+ * Return: div
+*/
+int div(int a, int b)
+{
+    return (a / b);
+}
